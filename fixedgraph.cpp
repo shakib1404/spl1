aabdd2f374e1bddd6cf11d01d9b506929794cbe3
shakib1404 <126335136+shakib1404@users.noreply.github.com>
2023-11-01 00:10:09 +0600

    Add files via upload

---
+++ fixedgraph.cpp
@@ -0,0 +1,332 @@
+#include "head.h"
+
+int fixed_x[MAX], fixed_y[MAX];
+int radius;
+int prevColor;
+
+void showFixedNodeDistance(int node, int distance)
+{
+    string label;
+
+    label = to_string(distance);
+    setcolor(LIGHTCYAN);
+    outtextxy(fixed_x[node]+6, fixed_y[node]-40, const_cast<char*>(label.c_str()));
+    delay(500);
+
+    setcolor(prevColor);
+}
+
+void showWeight ()
+{
+    int x, y;
+    string label;
+
+    settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
+
+    label = to_string(5);
+    x = (fixed_x[0] + fixed_x[1]) / 2 - 15;
+    y = (fixed_y[0] + fixed_y[1]) / 2 - 15;
+    outtextxy(x, y, const_cast<char*>(label.c_str()));
+    delay(500);
+
+    if (COUNT == 1){
+      label = to_string(-3);
+      x = (fixed_x[1] + fixed_x[2]) / 2;
+      y = (fixed_y[1] + fixed_y[2]) / 2 - 20;
+      outtextxy(x, y, const_cast<char*>(label.c_str()));
+      delay(500);
+    }
+    else {
+      label = to_string(4);
+      x = (fixed_x[1] + fixed_x[2]) / 2;
+      y = (fixed_y[1] + fixed_y[2]) / 2 - 20;
+      outtextxy(x, y, const_cast<char*>(label.c_str()));
+      delay(500);
+    }
+
+    label = to_string(3);
+    x = (fixed_x[2] + fixed_x[3]) / 2 + 15;
+    y = (fixed_y[2] + fixed_y[3]) / 2 - 10;
+    outtextxy(x, y, const_cast<char*>(label.c_str()));
+    delay(500);
+
+    label = to_string(2);
+    x = (fixed_x[3] + fixed_x[4]) / 2 + 10;
+    y = (fixed_y[3] + fixed_y[4]) / 2;
+    outtextxy(x, y, const_cast<char*>(label.c_str()));
+    delay(500);
+
+    if (COUNT == 1){
+      label = to_string(-2);
+      x = (fixed_x[4] + fixed_x[5]) / 2;
+      y = (fixed_y[4] + fixed_y[5]) / 2 + 5;
+      outtextxy(x, y, const_cast<char*>(label.c_str()));
+      delay(500);
+    }
+    else{
+      label = to_string(3);
+      x = (fixed_x[4] + fixed_x[5]) / 2;
+      y = (fixed_y[4] + fixed_y[5]) / 2 + 5;
+      outtextxy(x, y, const_cast<char*>(label.c_str()));
+      delay(500);
+    }
+
+    label = to_string(1);
+    x = (fixed_x[0] + fixed_x[5]) / 2 - 20;
+    y = (fixed_y[0] + fixed_y[5]) / 2 - 2;
+    outtextxy(x, y, const_cast<char*>(label.c_str()));
+    delay(500);
+
+    label = to_string(2);
+    x = (fixed_x[1] + fixed_x[5]) / 2 - 20;
+    y = (fixed_y[1] + fixed_y[5]) / 2;
+    outtextxy(x, y, const_cast<char*>(label.c_str()));
+    delay(500);
+
+    label = to_string(6);
+    x = (fixed_x[1] + fixed_x[6]) / 2 + 10;
+    y = (fixed_y[1] + fixed_y[6]) / 2 - 10;
+    outtextxy(x, y, const_cast<char*>(label.c_str()));
+    delay(500);
+
+    label = to_string(5);
+    x = (fixed_x[4] + fixed_x[6]) / 2 + 5;
+    y = (fixed_y[4] + fixed_y[6]) / 2 - 20;
+    outtextxy(x, y, const_cast<char*>(label.c_str()));
+    delay(500);
+
+    label = to_string(4);
+    x = (fixed_x[2] + fixed_x[4]) / 2 + 10;
+    y = (fixed_y[2] + fixed_y[4]) / 2;
+    outtextxy(x, y, const_cast<char*>(label.c_str()));
+    delay(500);
+}
+
+void fixedGraphConnection (int u, int v)
+{
+    setcolor(YELLOW);
+    setlinestyle(0, 0, 2);
+    line(fixed_x[u], fixed_y[u], fixed_x[v], fixed_y[v]);
+    delay(1600);
+
+    setcolor(prevColor);
+}
+
+void fixedGraphConnection_1 (int u, int v)
+{
+    setcolor(GREEN);
+    setlinestyle(0, 0, 2);
+    line(fixed_x[u], fixed_y[u], fixed_x[v], fixed_y[v]);
+    delay(1600);
+
+    setcolor(prevColor);
+}
+
+void fixedNodeColor (int u)
+{
+    setcolor(YELLOW);
+
+    string label = to_string(u);
+    outtextxy(fixed_x[u]-radius/2, fixed_y[u]-radius/2, const_cast<char*>(label.c_str()));
+    delay(1000);
+
+    setcolor(prevColor);
+}
+
+void fixedNodeColor_1 (int u)
+{
+    setfillstyle(SOLID_FILL, YELLOW);
+    fillellipse(fixed_x[u], fixed_y[u], 20, 20);
+
+    string label = to_string(u);
+    int textWidth = textwidth(const_cast<char*>(label.c_str()));
+    int textHeight = textheight(const_cast<char*>(label.c_str()));
+
+    setcolor(WHITE);
+    outtextxy(fixed_x[u]-textWidth/2, fixed_y[u]-textHeight/2, const_cast<char*>(label.c_str()));
+    //delay(1000);
+}
+
+void fixedNodeColor_2 (int u)
+{
+    setfillstyle(SOLID_FILL, GREEN);
+    fillellipse(fixed_x[u], fixed_y[u], 20, 20);
+
+    string label = to_string(u);
+    int textWidth = textwidth(const_cast<char*>(label.c_str()));
+    int textHeight = textheight(const_cast<char*>(label.c_str()));
+
+    setcolor(WHITE);
+    outtextxy(fixed_x[u]-textWidth/2, fixed_y[u]-textHeight/2, const_cast<char*>(label.c_str()));
+    //delay(1000);
+}
+
+void fixedNodeColor_3 (int u)
+{
+    setfillstyle(SOLID_FILL, LIGHTCYAN);
+    fillellipse(fixed_x[u], fixed_y[u], 20, 20);
+
+    string label = to_string(u);
+    int textWidth = textwidth(const_cast<char*>(label.c_str()));
+    int textHeight = textheight(const_cast<char*>(label.c_str()));
+
+    setcolor(WHITE);
+    outtextxy(fixed_x[u]-textWidth/2, fixed_y[u]-textHeight/2, const_cast<char*>(label.c_str()));
+    //delay(1000);
+}
+
+void Graph_FixedNodesAndEdges() {
+
+    int nodes = 7, edges = 10;
+
+    fixed_NODES = nodes;
+    fixed_EDGES = edges;
+
+    int adj_matrix[nodes][nodes]; // Adjacency matrix to store the graph
+
+    for (int i = 0; i < nodes; i++) {
+      for (int j = 0; j < nodes; j++) {
+        adj_matrix[i][j] = 0;
+
+        fixed_GRAPH[i][j] = 0;
+      }
+    }
+
+  //edge -> connection with vertices
+    adj_matrix[0][1] = 5;
+    adj_matrix[1][0] = 5;  // we have to Comment this line for directed graph
+    fixed_GRAPH[0][1] = fixed_GRAPH[1][0] = 5;
+
+    if (COUNT == 1){
+      adj_matrix[1][2] = -3;
+      adj_matrix[2][1] = -3;  // we have to Comment this line for directed fixed_GRAPH
+      fixed_GRAPH[1][2] = fixed_GRAPH[2][1] = -3;
+    }
+    else{
+      adj_matrix[1][2] = 4;
+      adj_matrix[2][1] = 4;  // we have to Comment this line for directed fixed_GRAPH
+      fixed_GRAPH[1][2] = fixed_GRAPH[2][1] = 4;
+    }
+
+
+    adj_matrix[2][3] = 3;
+    adj_matrix[3][2] = 3;  // we have to Comment this line for directed fixed_GRAPH
+    fixed_GRAPH[2][3] = fixed_GRAPH[3][2] = 3;
+
+    adj_matrix[3][4] = 2;
+    adj_matrix[4][3] = 2;  // we have to Comment this line for directed fixed_GRAPH
+    fixed_GRAPH[3][4] = fixed_GRAPH[4][3] = 2;
+
+    if(COUNT == 1) {
+      adj_matrix[4][5] = -2;
+    adj_matrix[5][4] = -2;  // we have to Comment this line for directed fixed_GRAPH
+    fixed_GRAPH[4][5] = fixed_GRAPH[5][4] = -2;
+    }
+    else{
+      adj_matrix[4][5] = 3;
+      adj_matrix[5][4] = 3;  // we have to Comment this line for directed fixed_GRAPH
+      fixed_GRAPH[4][5] = fixed_GRAPH[5][4] = 3;
+    }
+
+    adj_matrix[0][5] = 1;
+    adj_matrix[5][0] = 1;  // we have to Comment this line for directed fixed_GRAPH
+    fixed_GRAPH[0][5] = fixed_GRAPH[5][0] = 1;
+
+    adj_matrix[1][5] = 2;
+    adj_matrix[5][1] = 2;  // we have to Comment this line for directed fixed_GRAPH
+    fixed_GRAPH[1][5] = fixed_GRAPH[5][1] = 2;
+
+    adj_matrix[1][6] = 6;
+    adj_matrix[6][1] = 6;  // we have to Comment this line for directed fixed_GRAPH
+    fixed_GRAPH[1][6] = fixed_GRAPH[6][1] = 6;
+
+    adj_matrix[6][4] = 5;
+    adj_matrix[4][6] = 5;  // we have to Comment this line for directed fixed_GRAPH
+    fixed_GRAPH[6][4] = fixed_GRAPH[4][6] = 5;
+
+    adj_matrix[2][4] = 4;
+    adj_matrix[4][2] = 4;  // we have to Comment this line for directed fixed_GRAPH
+    fixed_GRAPH[2][4] = fixed_GRAPH[4][2] = 4;
+
+    setcolor(WHITE);
+    prevColor = getcolor();
+
+    setlinestyle(0, 0, 1);
+
+    // Draw fixed nodes as circles with numbers
+    radius = 20;
+
+    settextstyle(GOTHIC_FONT, HORIZ_DIR, 2);
+    string label;
+
+    //1st node
+    fixed_x[0] = 150;
+    fixed_y[0] = 300;
+    circle(fixed_x[0], fixed_y[0], radius);
+    label = to_string(0);
+    outtextxy(fixed_x[0]-radius/2, fixed_y[0]-radius/2, const_cast<char*>(label.c_str()));
+    delay(300);
+
+    //2nd node
+    fixed_x[1] = 330;
+    fixed_y[1] = 150;
+    circle(fixed_x[1], fixed_y[1], radius);
+    label = to_string(1);
+    outtextxy(fixed_x[1]-radius/2, fixed_y[1]-radius/2, const_cast<char*>(label.c_str()));
+    delay(300);
+
+    //3rd node
+    fixed_x[2] = 330 + 220;
+    fixed_y[2] = 150;
+    circle(fixed_x[2], fixed_y[2], radius);
+    label = to_string(2);
+    outtextxy(fixed_x[2]-radius/2, fixed_y[2]-radius/2, const_cast<char*>(label.c_str()));
+    delay(300);
+
+    //4th node
+    fixed_x[3] = 330 + 220 + 180;
+    fixed_y[3] = 300;
+    circle(fixed_x[3], fixed_y[3], radius);
+    label = to_string(3);
+    outtextxy(fixed_x[3]-radius/2, fixed_y[3]-radius/2, const_cast<char*>(label.c_str()));
+    delay(300);
+
+    //5th node
+    fixed_x[4] = 330 + 220;
+    fixed_y[4] = 450;
+    circle(fixed_x[4], fixed_y[4], radius);
+    label = to_string(4);
+    outtextxy(fixed_x[4]-radius/2, fixed_y[4]-radius/2, const_cast<char*>(label.c_str()));
+    delay(300);
+
+    //6th node
+    fixed_x[5] = 330;
+    fixed_y[5] = 450;
+    circle(fixed_x[5], fixed_y[5], radius);
+    label = to_string(5);
+    outtextxy(fixed_x[5]-radius/2, fixed_y[5]-radius/2, const_cast<char*>(label.c_str()));
+    delay(300);
+
+    //7th node
+    fixed_x[6] = 440;
+    fixed_y[6] = 300;
+    circle(fixed_x[6], fixed_y[6], radius);
+    label = to_string(6);
+    outtextxy(fixed_x[6]-radius/2, fixed_y[6]-radius/2, const_cast<char*>(label.c_str()));
+
+    // Wait for a delay before proceeding to the next step
+    delay(1000);
+
+    // Draw edges as lines
+    for (int i = 0; i < nodes; i++) {
+      for (int j = i+1; j < nodes; j++) {
+        if (adj_matrix[i][j] != 0) {
+          line(fixed_x[i], fixed_y[i], fixed_x[j], fixed_y[j]);
+          delay(300);
+        }
+      }
+    }
+
+
+}
+
---
+++ primsalgo.cpp
@@ -0,0 +1,183 @@
+#include "head.h"
+
+int grapH[MAX][MAX];
+int nodes_P, edges_P;
+
+int selectMinVertex (vector<int>& value, vector<bool>& setMST)
+{
+	int minimum = INT_MAX;
+	int vertex;
+
+	for (int i = 0; i < nodes_P; i++)
+	{
+		if (setMST[i] == false && value[i] < minimum)
+		{
+			vertex = i;
+			minimum = value[i];
+		}
+	}
+
+	return vertex;
+}
+
+int findMST (int grapH[MAX][MAX])
+{
+	int parent[nodes_P], sum = 0;
+	vector<int> value (nodes_P, INT_MAX); //used edged relaxation
+	vector<bool> setMST (nodes_P, false); //TRUE -> vertex is included in MST
+
+	//Assuming start point as Node -> 0
+	parent[0] = -1; // start node has no parent
+	value[0] = 0; // start node has value = 0 to get picked 1st
+
+	//from MST with (v-1) edges
+	for (int i = 0; i < nodes_P-1; i++)
+	{
+		//select best vertex applying greedy method
+		int u = selectMinVertex (value, setMST);
+
+		setMST[u] = true; //include new vertex in MST
+
+		//relax adjacent vertices (noy yet included in MST)
+		for (int j = 0; j < nodes_P; j++)
+		{
+
+			if (grapH[u][j] != 0 && setMST[j] == false && grapH[u][j] < value[j])
+			{
+				value[j] = grapH[u][j];
+				parent[j] = u;
+			}
+		}
+	}
+
+	//print MST
+	for (int i = 1; i < nodes_P; i++)
+	{
+		cout << "u -> v : " << parent[i] << "->" << i << ", weight = " << grapH[parent[i]][i] << endl;
+
+		if (CHOOSE == 1) fixedGraphConnection(parent[i], i); // for fixed graph
+        else if (CHOOSE == 2) graphConnection(parent[i], i); // for random graph
+        else graphConnection(parent[i], i); // for user input graph
+
+		if (CHOOSE == 1) fixedNodeColor_1 (parent[i]); // for fixed graph
+        else if (CHOOSE == 2) nodeColor_1 (parent[i]); //for random graph
+        else nodeColor_1 (parent[i]); // for user input graph
+
+        if (CHOOSE == 1) fixedNodeColor_1 (i); // for fixed graph
+        else if (CHOOSE == 2) nodeColor_1 (i); //for random graph
+        else nodeColor_1 (i); // for user input graph
+
+		delay(1000);
+	}
+
+	for (int i = 1; i < nodes_P; i++)
+	{
+		sum += grapH[parent[i]][i];
+	}
+
+	return sum;
+
+}
+
+void MST_Prims_Algorithm ()
+{
+	// settextstyle(GOTHIC_FONT, HORIZ_DIR, 3);
+    // outtextxy(400, 30, "Prims");
+
+	// Graph_FixedNodesAndEdges();
+
+	setfillstyle(SOLID_FILL, BLACK); // to erase the line "Please press any key to continue"
+    bar(230, 500, 230 + 500, 500 + 600); // draws a rectangle over the text
+
+	circle(715, 70, 15);
+    setfillstyle(SOLID_FILL, YELLOW);
+    fillellipse(715, 70, 15, 15);
+    outtextxy(735, 47, "minimum");
+    outtextxy(735, 68, "spanning tree");
+
+	if (CHOOSE == 1){
+        //for Fixed Graph
+        nodes_P = fixed_NODES;
+        edges_P = fixed_EDGES;
+
+        for (int i = 0; i < nodes_P; i++){
+            for (int j = 0; j < nodes_P; j++){
+                grapH[i][j] = fixed_GRAPH[i][j];
+            }
+        }
+
+    }
+
+    else if (CHOOSE == 2){
+		//for Random Input
+        nodes_P = NODES;
+        edges_P = EDGES;
+
+        int k = 0;
+        for (int i = 0; i < nodes_P; i++){
+            for (int j = 0; j < nodes_P; j++){
+                grapH[i][j] = GRAPH[i][j];
+            }
+        }
+
+    }
+
+    else {
+        //for User Input
+        nodes_P = NODES;
+        edges_P = EDGES;
+
+        int k = 0;
+        for (int i = 0; i < nodes_P; i++){
+            for (int j = 0; j < nodes_P; j++){
+                grapH[i][j] = GRAPH[i][j];
+            }
+        }
+
+    }
+
+
+	cout << "MST cost is : " << findMST(grapH) << endl;
+
+	settextstyle(GOTHIC_FONT, HORIZ_DIR, 2);
+    outtextxy(300, 550, "Successfully ended...");
+
+
+	prevWindowArrow();
+
+    int x = mousex();
+    int y = mousey();
+    if (ismouseclick(WM_LBUTTONDOWN)) {
+        clearmouseclick(WM_LBUTTONDOWN); // clear the mouse click event
+        if (x > 50 && x < 110 && y > 50 && y < 90) {
+            // button was clicked
+            cleardevice();
+            delay(100);
+            //getch();
+            showList();
+            //break;
+        }
+    }
+
+}
+
+//time complexity : O(V^2)
+//if we use adjacency list : O(E log V)
+/*
+9
+14
+0 1 4
+1 2 8
+2 3 7
+3 4 9
+4 5 10
+5 6 2
+6 7 1
+7 8 7
+0 7 8
+1 7 11
+2 8 2
+6 8 6
+2 5 4
+3 5 14
+*/
